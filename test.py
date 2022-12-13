bb806eb84248047802eebf0d1c98ea260d378ceb
Jonny Gal <jonnycwgal@gmail.com>
2022-12-12 22:13:43 -0800

    Simple server

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import tekore as tk
 
 client_id = 'fd2f22d376b146cea9104d032130f655'
 client_secret = '94c642a934f14c6aaab4eea568b0a8f5'
-redirect_uri = 'https://localhost:5000/callback/'
+redirect_uri = 'http://localhost:5000/callback/'
 
 user_token = tk.prompt_for_user_token(
     client_id,
